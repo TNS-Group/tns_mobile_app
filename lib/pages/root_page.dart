8a3526d9b115a6d8ea18f78a202212b982436be9
1h3j <221726394+1h3j@users.noreply.github.com>
2025-12-10 16:30:50 +0800

    aklsdfkldafjfljdsajdfl

--- lib/pages/root_page.dart
+++ lib/pages/root_page.dart
@@ -915,72 +915,70 @@ class _TNSRootPageState extends State<TNSRootPage> with TickerProviderStateMixin
 
         Container(
           padding: EdgeInsets.all(16),
-          child: Expanded(
-            child: Stack(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(16),
-                  child: ListView(
-                    shrinkWrap: true,
-                    children: (sortedSchedules.isEmpty) ? [ Center(
-                      child: const Text(
-                        "Woohoo! You have no upcoming classes", 
-                        style: TextStyle(fontStyle: FontStyle.italic,
-                        ),
+          child: Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: ListView(
+                  shrinkWrap: true,
+                  children: (sortedSchedules.isEmpty) ? [ Center(
+                    child: const Text(
+                      "Woohoo! You have no upcoming classes", 
+                      style: TextStyle(fontStyle: FontStyle.italic,
                       ),
-                    )] : [ for (Schedule i in sortedSchedules)
-                      if (i.weekday == selectedDay)
-                      Padding(
-                        padding: EdgeInsetsGeometry.only(bottom: 8), 
-                        child: Ink(
-                          child: InkWell( 
-                            splashColor: Colors.blue,
-                            onTap: () {
-                              showScheduleEditPopup(context, i.id);
-                            },
-                            child: Stack(
-                              fit: StackFit.loose,
-                              children: [
-                                ScheduleItem(
-                                  start: i.timeIn,
-                                  end: i.timeOut,
-                                  className: classes[i.classId]?.name ?? '',
-                                  subject: i.subject,
-                                  weekday: i.weekday,
-                                ),
-                                Positioned.fill(
-                                  child: Align(
-                                    alignment: Alignment.bottomRight,
-                                    child: Padding( 
-                                      padding: EdgeInsetsGeometry.all(16),
-                                      child: Icon(
-                                        Icons.edit,
-                                      )
+                    ),
+                  )] : [ for (Schedule i in sortedSchedules)
+                    if (i.weekday == selectedDay)
+                    Padding(
+                      padding: EdgeInsetsGeometry.only(bottom: 8), 
+                      child: Ink(
+                        child: InkWell( 
+                          splashColor: Colors.blue,
+                          onTap: () {
+                            showScheduleEditPopup(context, i.id);
+                          },
+                          child: Stack(
+                            fit: StackFit.loose,
+                            children: [
+                              ScheduleItem(
+                                start: i.timeIn,
+                                end: i.timeOut,
+                                className: classes[i.classId]?.name ?? '',
+                                subject: i.subject,
+                                weekday: i.weekday,
+                              ),
+                              Positioned.fill(
+                                child: Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: Padding( 
+                                    padding: EdgeInsetsGeometry.all(16),
+                                    child: Icon(
+                                      Icons.edit,
                                     )
                                   )
                                 )
-                              ],
-                            )
+                              )
+                            ],
                           )
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Positioned(
-                  bottom: 1,
-                  right: 1,
-                  child: FilledButton(
-                    onPressed: () { showScheduleAddPopup(context); }, 
-                    child: const Padding(
-                      padding: EdgeInsetsGeometry.symmetric(vertical: 16, horizontal: 32),
-                      child: Text("+ Add New")
+                        )
+                      ),
                     )
-                  ),
-                )
-              ],
-            )
-          ),
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: 1,
+                right: 1,
+                child: FilledButton(
+                  onPressed: () { showScheduleAddPopup(context); }, 
+                  child: const Padding(
+                    padding: EdgeInsetsGeometry.symmetric(vertical: 16, horizontal: 32),
+                    child: Text("+ Add New")
+                  )
+                ),
+              )
+            ],
+          )
         ),
       ],
     );
