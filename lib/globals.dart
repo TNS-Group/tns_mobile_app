2c6d6aa2340323653a2eb0771c0e04638cad24c2
) <macbookpro@s-MacBook-Pro.local>
2025-12-10 16:24:13 +0800

    Some

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 
-String baseURL = kDebugMode ? 
-  "http://127.0.0.1:8000":
-  "https://tns.tnsgroup.win";
+String baseURL = "https://tns.tnsgroup.win";
+// String baseURL = kDebugMode ? 
+//   "http://127.0.0.1:8000":
+//   "https://tns.tnsgroup.win";
